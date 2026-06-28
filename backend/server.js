284b9ee9fdd7204cfc30125b026aa85c36425e77
AndreyGlitet <165326453+AndreyGlitet@users.noreply.github.com>
2026-06-28 11:31:03 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const __dirname = dirname(__filename);
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
